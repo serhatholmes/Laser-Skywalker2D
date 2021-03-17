e8a8afb53700520cb0f856d6d324861a7c0e6693
serhatholmes <serhatholmes9703@hotmail.com>
2021-03-17 14:13:22 +0300

    All files
    Commiting Files

---
+++ Laser Skywalker/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    Text healthText;
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthText = GetComponent<Text>();
+        player = FindObjectOfType<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = player.GetHealth().ToString();
+    }
+}
---
+++ Laser Skywalker/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    Text scoreText;
+    GameSession gameSession;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = gameSession.GetScore().ToString();
+    }
+}
